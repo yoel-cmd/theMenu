00496babe977e8b17a3b2bf952bad57ff0e34017
yoel <yoeli5746@gmail.com>
2025-05-13 18:12:03 +0300

    finish

--- theMenu/Program.cs
+++ theMenu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -110,24 +111,90 @@ namespace theMenu
         }
 
 
-
-
-
-            // בדיקה של קליטת בחירה מהמשתמש 
-            static void coise()
+        //Checking if everything is numbers
+        static bool checkIfNumbers(string[] inputArr)
         {
+            if (inputArr.Length < 3)
+            {
+                Console.WriteLine("error 1");
+                return false;
+            }
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                try
+                {
+                   int num = Convert.ToInt32(inputArr[i]);
+                }
+                catch
+                {
+                    Console.WriteLine("error 2 ");
+                    return false;
+                }
+            }
+            return true;
+        }
+        // checked if there are at least 3 numbers  
+        //static bool CheckThreePositive(int[] inputArr)
+        //{
+        //    if (inputArr.Length < 3)
+        //    {
+        //        return false;
+        //    }
+            
+        //    return false;
+        //}
 
-            int[] arrNum = { 33, 52, 3, 14, 25, 66, 7 };
-            PrintArrayInOrder(arrNum);
+
+
+        //function input user
+        static string[] userInput()
+        {
+            
+            Console.WriteLine("Enter a series of at least 3 numbers with a space between them.");
+            string[] inputStringArr = Console.ReadLine().Split(' ');
+
+            return inputStringArr;
+        }
+
+
+
+
+        
+
+
+
+
+        static string[] menu(string[] inputStringArr)
+        {
+            //Receiving a series of numbers from the user
+            int[] originalInputArr = new int[inputStringArr.Length];
+
+            //Check if all are numbers if so then check if at least size 3 if so then at least 3 are positive
+            
+            
+                for (int i = 0; i < inputStringArr.Length; i++)
+                {
+                    originalInputArr[i] = Convert.ToInt32(inputStringArr[i]);
+                }
+
+
+
+            bool flag =true;
+
+            //int[] arrNum = { 33, 52, 3, 14, 25, 66, 7 };
+            //PrintArrayInOrder(arrNum);
 
             Console.WriteLine(" ");
 
-            // Sorting the array while preserving the original array
-            int[] arrNumSort = sortArr((int[])arrNum.Clone());
-            PrintArrayInOrder(arrNumSort);
-            while (true)
+            //Sorting the array while preserving the original array
+            int[] sortedInputArr = sortArr((int[])originalInputArr.Clone());
+
+
+
+
+            while (flag)
             {
-                // לשנות את המערך לduble כי רק ככה אני יכול לבדוק שהוא מספר - ולדאוג שיכניס את זה עם רווחים
+
                 Console.WriteLine(
                     "Enter your choice:\n" +
                     "To enter a series, press 1\n" +
@@ -140,63 +207,89 @@ namespace theMenu
                     "To display the number of elements, press 8\n" +
                     "To display the sum of the series, press 9\n" +
                     "To exit, press 10"
-                ); 
-                double num = Convert.ToInt32(Console.ReadLine());
+                );
+
+
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num == 10)
+                {
+                    Console.WriteLine("exit \n");
+                    flag = false;
+                    continue;
+                }
 
                 Console.WriteLine(" ");
 
                 switch (num)
                 {
                     case 1:
-                        Console.WriteLine("Enter a series");
+                       Console.WriteLine("Okay, you chose to enter the series of numbers from the beginning. \n");
+                       inputStringArr=  inputTrue(userInput());
+                        flag = false;
+                        Console.WriteLine("Error ");
+
                         break;
                     case 2:
-                        PrintArrayInOrder(arrNum);
+                        PrintArrayInOrder(originalInputArr);
                         break;
                     case 3:
-                        Console.WriteLine("Display in reverse order");
-                        PrintArrayReversOrder(arrNum);
+                        Console.WriteLine("Display in reverse order \n");
+                        PrintArrayReversOrder(originalInputArr);
                         break;
                     case 4:
                         Console.WriteLine("Sorted display");
-                        printSortArr(arrNumSort);
+                        printSortArr(sortedInputArr);
                         break;
                     case 5:
-                        Console.WriteLine("Show the highest number");
-                        printMaxItem(arrNumSort);
+                        Console.WriteLine("Show the highest number \n");
+                        printMaxItem(sortedInputArr);
                         break;
                     case 6:
-                        Console.WriteLine("Displaying the smallest number");
-                        printMinItem(arrNumSort);
+                        Console.WriteLine("Displaying the smallest number \n");
+                        printMinItem(sortedInputArr);
                         break;
                     case 7:
-                        Console.WriteLine("Showing the average");
-                        printAvgItem(arrNum);
+                        Console.WriteLine("Showing the average \n");
+                        printAvgItem(sortedInputArr);
                         break;
                     case 8:
-                        Console.WriteLine("Displaying the number of organs");
-                        printCountElements(arrNum);
+                        Console.WriteLine("Displaying the number of organs \n");
+                        printCountElements(sortedInputArr);
                         break;
                     case 9:
-                        Console.WriteLine("Show the series sum");
-                        printSumItem(arrNum);
+                        Console.WriteLine("Show the series sum \n");
+                        printSumItem(sortedInputArr);
                         break;
-                    case 10:
-                        Console.WriteLine("exit");
-                        break;
-                    default:
-                        Console.WriteLine("No choice was made difficult.");
+                    default :
+                        Console.WriteLine("Does not comply with the writing rules");
                         break;
                 }
+                
             }
+            Console.WriteLine("Error ");
+            return inputStringArr;
+        }
+         static string[] inputTrue(string[] input)
+        {
+            while (!checkIfNumbers(input))
+            {
+                input = userInput();
+
+            }
+            return input;
         }
 
 
         // main
         static void Main(string[] args)
         {
-            coise();
+            while (true)
+            {
+                args = inputTrue(args);
 
+                args = menu(args);
+
+            }
         }
     }
 }
